610e38db0f3df7ae037b1a8ba564828d6272e940
Yandra Muslim <yandrayt1@gmail.com>
2023-07-20 13:24:57 +0700

    fix: :zap:

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
               path="*"
               element={
                 <div className="w-full h-full flex items-center justify-center">
-                  <h1 className="text-4xl font-bold">NotFound</h1>
+                  <h1 className="text-4xl font-bold">404 NotFound</h1>
                 </div>
               }
             />
