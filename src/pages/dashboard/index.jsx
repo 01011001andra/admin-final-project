b7135a1fdb4e09c8bdebf3436b8754274c52f587
Yandra Muslim <yandrayt1@gmail.com>
2023-07-22 10:31:35 +0700

    fix(Dashboard): :art: error doesnt fix

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { Layout, TableSection } from "../../layouts";
-import { Card, KasTabel } from "./components";
+import { Layout } from "../../layouts";
+import { useGetKas } from "../../lib/kas";
+import { TabelKas } from "../keuangan/components";
+import { Card } from "./components";
 
 const Dashboard = () => {
+  const {
+    data: kasData,
+    isError: kasIsErr,
+    error: kasErr,
+    isLoading: kasIsLoad,
+  } = useGetKas();
+
   return (
     <Layout title="Dashboard">
       <div className="grid grid-cols-1 gap-3 lg:gap-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
@@ -10,23 +19,7 @@ const Dashboard = () => {
         <Card title="ceramah" link="/ceramah" />
         <Card title="keuangan & e-infak" link="/keuangan" />
       </div>
-      <div className="flex flex-col w-full gap-5">
-        <TableSection
-          title="Laporan Keuangan Masjid Al-Ihsan"
-          description="Uang digunakan untuk keperluan masjid Al-Ihsan"
-        />
-        <KasTabel />
-        <div className="flex justify-start w-full gap-4">
-          <div className="grid grid-cols-2 join ">
-            <button className="join-item btn btn-outline hover:bg-stroke">
-              Previous page
-            </button>
-            <button className="join-item btn btn-outline hover:bg-stroke">
-              Next page
-            </button>
-          </div>
-        </div>
-      </div>
+      <TabelKas getData={kasData} error={kasErr} isError={kasIsErr} />
     </Layout>
   );
 };
